dc52dd5fe426b085913f6687dab394bc7cb10ed5
lnuvy <hangeulgod@naver.com>
2022-01-24 08:33:23 +0900

    tsc-watch 셋업

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const name = "lnuvy",
+  age = 24,
+  gender = "male";
+
+  const sayHi = (name: string, age: number, gender: string): void => {
+    console.log(`Hello ${name}, You are ${age}, You are a ${gender}`);
+  }
+
+  sayHi("Lnuvy", 24, "male");
+
+  export {};
