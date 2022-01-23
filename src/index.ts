85baf662008c1deea870f66ff91e34c2d0476e1d
lnuvy <hangeulgod@naver.com>
2022-01-24 08:44:34 +0900

    interface 는 컴파일 되지 않음, class 로 선언시 컴파일됨

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name:string, age:number, gender?:string){
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "lnuvy",
-  age: 24,
-  gender: "male",
-}
+const lnuvy = new Human("lnuvy", 123, "male")
 
   const sayHi = (person: Human): void => {
     console.log(`Hello ${person.name}, You are ${person.age}, You are a ${person.gender}!!!`);
   }
 
-  sayHi(person);
+  sayHi(lnuvy);
 
   export {};
