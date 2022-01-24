a4510778aebb4795edf0c4c366e40d3a0a44d3fc
lnuvy <hangeulgod@naver.com>
2022-01-24 09:09:07 +0900

    yarn add crypto-js, static Method (자바와 거의 동일), 간단한 함수작성

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,43 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
     previousHash: string,
     data: string,
     timestamp: number
-    ){
-      this.index = index;
-      this.hash = hash;
-      this.previousHash = previousHash;
-      this.data = data;
-      this.timestamp = timestamp;
-    }
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
 const genesisBlock: Block = new Block(0, "123123123", "", "hello", 123456);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => getBlockchain[blockchain.length - 1];
 
-  export {};
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+export {};
